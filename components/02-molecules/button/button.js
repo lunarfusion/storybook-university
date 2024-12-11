54f13ab6b92b1b4021bdeaedf12e653a0b4c729e
lunarfusion <monet@lunarfusion.com>
2024-12-11 11:51:19 -0500

    fix wip: main menu

--- components/02-molecules/button/button.js
+++ components/02-molecules/button/button.js
@@ -2,38 +2,38 @@
 
   // wrap in DOM load so Storybook does not throw errors
   document.addEventListener('DOMContentLoaded', function () {
-    const menuTrigger = document.getElementById("menu-toggle");
-      ['mousedown', 'focus.keydown'].forEach(event => menuTrigger.addEventListener(event, showHideMenu));
+    // const menuTrigger = document.getElementById("menu-toggle");
+    //   ['mousedown', 'focus.keydown'].forEach(event => menuTrigger.addEventListener(event, showHideMenuToggler));
+    //   
+    //   function showHideMenuToggler(event) {
+    //     event.preventDefault();
+    //     // change toggle button to is-open
+    //     menuTrigger.classList.toggle("is-open");
+    //   };  
       
-      function showHideMenu(event) {
-        event.preventDefault();
-        // change toggle button to is-open
-        menuTrigger.classList.toggle("is-open");
-      };  
-      
-      // Create form wrapper object - this contains the form being opened/closed
-      const mainMenu = document.getElementById("main-menu-hider");
-      
-        function showHideMenu(event) {
-          
-          // preventDefault so focus does not fire on click
-          event.preventDefault();
-        
-          // Search-open removes the closed transform from the form it contains
-          mainMenu.classList.toggle("menu-opened");
-          mainMenu.classList.toggle("menu-closed");
-        
-          // change toggle button to is-open
-          menuTrigger.classList.toggle("is-open");
-        
-          // aria-expanded switch
-          // get the aria-expanded state of the toggler and if it's true, make it false
-          if (mainMenu.getAttribute("aria-expanded") === "true") {
-            mainMenu.setAttribute("aria-expanded", "false");
-          } else {
-            // otherwise if it's false, make it true
-            mainMenu.setAttribute("aria-expanded", "true");
-          };
-        }; 
+      // // Create form wrapper object - this contains the form being opened/closed
+      // const mainMenu = document.getElementById("main-menu-hider");
+      // 
+      //   function showHideMenu(event) {
+      //     
+      //     // preventDefault so focus does not fire on click
+      //     event.preventDefault();
+      //   
+      //     // Search-open removes the closed transform from the form it contains
+      //     mainMenu.classList.toggle("menu-opened");
+      //     mainMenu.classList.toggle("menu-closed");
+      //   
+      //     // change toggle button to is-open
+      //     menuTrigger.classList.toggle("is-open");
+      //   
+      //     // aria-expanded switch
+      //     // get the aria-expanded state of the toggler and if it's true, make it false
+      //     if (mainMenu.getAttribute("aria-expanded") === "true") {
+      //       mainMenu.setAttribute("aria-expanded", "false");
+      //     } else {
+      //       // otherwise if it's false, make it true
+      //       mainMenu.setAttribute("aria-expanded", "true");
+      //     };
+      //   }; 
   });
 })(Drupal);
